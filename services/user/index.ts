5f1774909aa822a1300a341c82968d27ea58c3ad
Natchapol Srisang <utopiabeam@gmail.com>
2022-07-14 15:12:01 +0700

    feat(user): add user service

---
+++ services/user/index.ts
@@ -0,0 +1,11 @@
+import { ApolloServer } from 'apollo-server'
+import { createGqlServer } from 'libs/server'
+import { resolvers } from './resolvers'
+import { typeDefs } from './resolvers/schema'
+
+export function createServer(): Promise<ApolloServer> {
+  return createGqlServer({
+    typeDefs,
+    resolvers,
+  })
+}
